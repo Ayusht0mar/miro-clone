00175c3df3def5b1d8f35c5d9058820c39550ac9
Ayush tomar <ayushtomar388@gmail.com>
2025-01-01 23:59:12 +0530

    Navbar created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,31 @@
 import { SignIn } from "@/components/auth/SignInButton";
 import { SignOut } from "@/components/auth/SignOutButton";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className="pt-6">
+      <div className="bg-white mx-auto w-[92vw] p-4 rounded-xl flex justify-between items-center shadow-lg ">
+        <div className="flex gap-4 items-center">
+          <Image
+            src={"/images/mirosymbol.svg"}
+            alt="Miro Symbol"
+            height={40}
+            width={40}
+          />
+          <Image
+            src={"/images/mirologo.svg"}
+            alt="Miro logo"
+            height={40}
+            width={72}
+          />
+        </div>
+        <div>
+          <a href="/signin" className="text-white bg-blue-500 px-4 py-2 rounded-lg font-semibold">   
+              Sign in
+          </a>
+        </div>
+      </div>
 
 <SignIn/>
 <SignOut/>
