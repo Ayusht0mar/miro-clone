2cb1d81b85355daf4446d65f268229b6ffa0b2a4
Ayush tomar <ayushtomar388@gmail.com>
2025-01-02 23:46:16 +0530

    signin page protected from logged in users

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,12 +1,16 @@
+import { auth } from "@/auth";
 import { SignIn } from "@/components/auth/SignInButton";
+import { redirect } from "next/navigation";
 
-const SignInPage = () => {
+export default async function SignInPage () {
+        
+    const session = await auth()
+    if (session) return redirect("/dashboard/")
+
     return ( 
         <div className="w-full h-full min-h-screen flex flex-col items-center justify-center">
             <h1 className="text-2xl mb-6">Sign In Page</h1>
             <SignIn/>
         </div>
      );
-}
- 
-export default SignInPage;
+} 
