46eb765a7c047d1d471b6930dc6d8dced26e8db7
Ayush tomar <ayushtomar388@gmail.com>
2025-01-08 23:12:08 +0530

    board page created

--- src/app/board/[boardId]/page.tsx
+++ src/app/board/[boardId]/page.tsx
@@ -1,9 +1,31 @@
-const BoardPage = () => {
-    return ( 
-        <div>
-            Board Page
-        </div>
-     );
-}
- 
-export default BoardPage;
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+import Canvas from "./_components/Canvas";
+import { Room } from "./_components/Room";
+
+interface BoardIdPageProps {
+    params: Promise<{ boardId: string }>;
+  }
+  
+  export default async function BoardPage({ params }: BoardIdPageProps) {
+    const resolvedParams = await params;
+    const { boardId } = resolvedParams;
+  
+    const session = await auth();
+    if (!session?.user) {
+      return redirect("/signin");
+    }
+  
+    if (!boardId) {
+      return redirect("/boards"); // Fallback route
+    }
+  
+    return (
+      <main>
+        <Room roomId={boardId}>
+          <Canvas boardId={boardId} />
+        </Room>
+      </main>
+    );
+  }
+  
