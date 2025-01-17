1772e49bbedcee253247e2ae4f5abb92b52ad9bf
Ayush tomar <ayushtomar388@gmail.com>
2025-01-17 17:24:02 +0530

    Styled and changed to function

--- src/components/dashboard/DashboardSidebar.tsx
+++ src/components/dashboard/DashboardSidebar.tsx
@@ -1,12 +1,16 @@
+import { auth } from "@/auth";
 import { Clock, House, Search, Star } from "lucide-react";
 
-const DashboardSidebar = () => {
+export default async function DashboardSidebar () {
+
+        const session = await auth();
+
     return ( 
-        <div className="bg-white w-1/5 min-h-screen max-w-60 p-3 border-r border-gray-100">
-            <p className="p-2 border rounded-md mb-3">Team Name</p>
+        <div className="hidden lg:block bg-white w-1/5 min-h-screen max-w-60 p-3 border-r border-gray-100">
+            <p className="p-2 border rounded-md mb-3">{session?.user?.name}&apos;s Board</p>
             <p className="p-2 rounded-md bg-zinc-100 text-zinc-500 flex gap-2 items-center"> <Search size={18}/> Search</p>
             <div className="flex flex-col gap-1 mt-4">
-                <div className="flex gap-2 items-center hover:bg-slate-100 p-2 rounded-md">
+                <div className="flex gap-2 items-center hover:bg-slate-100 p-2 rounded-md bg-zinc-100 text-zinc-700">
                     <House size={20}/>
                     Home 
                 </div>
@@ -22,5 +26,3 @@ const DashboardSidebar = () => {
         </div>
      );
 }
- 
-export default DashboardSidebar;
